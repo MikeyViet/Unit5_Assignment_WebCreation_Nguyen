cb57a268e4fa064d05698905be00811d11eb3925
cuviet <vietwiin@gmail.com>
2022-06-23 22:44:32 -0500

    Add project files.

---
+++ Unit5_VS2022_Assignment_Nguyen/Pages/Privacy.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
+
+namespace Unit5_VS2022_Assignment_Nguyen.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            string dateTime = DateTime.Now.ToString("d", new CultureInfo("en-US"));
+            ViewData["TimeStamp"] = dateTime; 
+
+        }
+    }
+}
